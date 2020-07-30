51a99153cb9c2e3be0a611da18c8c838d29d0984
Satyajit Paul <37581487+satyajit99@users.noreply.github.com>
2020-07-30 22:55:10 +0530

    Add files via upload

---
+++ CreateTeamsBulk.ps1
@@ -0,0 +1,35 @@
+﻿#Connnect to Microsoft Teams
+$connection = Connect-MicrosoftTeams
+
+
+try
+{
+    #CSV File Path. Change this location accordingly
+    $filePath = "C:\Users\Satya\Desktop\CreateBulkTeams.csv"
+
+    #read the input file
+    $loadFile = Import-Csv -Path $filePath
+    
+    
+    foreach ($row in $loadFile)
+    {
+        $teamName = $row.'Team name'
+        $teamDescription = $row.'Team description'
+        $teamOwner = $row.Owner
+        $teamVisibility = $row.Visibility
+
+        #create the team with specified parameters
+        $groupID = New-Team -DisplayName $teamName -Owner $teamOwner -Description $teamDescription -Visibility $teamVisibility
+        Write-Host "Team " $teamName " created successfully..."
+    }
+    
+    Write-Host $loadFile.Count " teams were created" -ForegroundColor Green -BackgroundColor Black
+  
+}
+
+catch
+{
+      Write-Host "An error occurred:"
+      Write-Host $_
+}
+
